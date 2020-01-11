84e8209eb74de0fe291eda7eea5b561956bf9b98
quentinlallemand <quentinlallemand@gmail.com>
2020-01-11 11:00:13 +0100

    coucou

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,3 +18,6 @@
 console.log('Hello World from Webpacker')
 
 import 'bootstrap';
+import { test } from './components/ingredient-form'; // <-- add this line
+
+test()
--- app/javascript/packs/components/ingredient-form.js
+++ app/javascript/packs/components/ingredient-form.js
@@ -1,3 +1,14 @@
-let button = Document.querySelector('#add-ingredient');
 
-const clickButton = button.addEventLister("click",  )
+const test = () => {
+
+  const button = document.querySelector('.add-ingredient');
+
+  const clickButton = button.addEventListener('click', (event) => {
+    const newDose = document.querySelector('#new-dose');
+    newDose.classList.remove('unvisible');
+    console.log('toto')
+  });
+
+}
+
+export { test };
